f52e5113f409f20190cc5f664252ea0a715c908a
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-08-13 18:10:35 +0530

    on-after-errors-13/08

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CouponComponent } from './coupon/coupon.component';
 import { InventoryComponent } from './inventory/inventory.component';
 
 const routes: Routes = [{
@@ -10,6 +11,10 @@ const routes: Routes = [{
     {
       path:"",
       component:InventoryComponent
+    },
+    {
+      path:'coupon',
+      component:CouponComponent
     }
   ]
 }];
